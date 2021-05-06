e3852cc81a070ae196ba2f16f3eda02b032904f6
Mohammed Elamine <51944814+mohammed-esi@users.noreply.github.com>
2021-05-06 18:40:39 +0200

    add loaduser on server forntend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { LOGOUT } from './utils/constant';
+
+import setAuthToken from './utils/setAuthToken';
 
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
+import { loadUser } from './actinos/auth';
 
 import Routes from './components/routing/Routes';
 
@@ -11,6 +15,18 @@ import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 
 function App() {
+  useEffect(() => {
+    // check for token in LS
+    if (localStorage.token) {
+      setAuthToken(localStorage.token);
+    }
+    store.dispatch(loadUser());
+
+    // log user out from all tabs if they log out in one tab
+    window.addEventListener('storage', () => {
+      if (!localStorage.token) store.dispatch({ type: LOGOUT });
+    });
+  }, []);
   return (
     <Provider store={store}>
       <Router>
