cb04c7c64717cab6a4f018db3961e9d43751191d
Mohammed Elamine <51944814+mohammed-esi@users.noreply.github.com>
2021-05-06 18:44:53 +0200

    add constant of appointment

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,18 @@
-// User Ptient
+// User Constant
+export const USER_LOADED = 'USER_LOADED';
+export const AUTH_ERROR = 'AUTH_ERROR';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGOUT = 'LOGOUT';
+export const SET_ALERT = 'SET_ALERT';
+export const REMOVE_ALERT = 'REMOVE_ALERT';
+export const CLEAR_ERROR = 'CLEAR_ERROR';
+
+// Patients Constant
 export const GET_PATIENTS = 'GET_PATIENTS';
 export const PAITENT_ERROR = 'PAITENT_ERROR';
 export const CREATE_PATIENT = 'CREATE_PATIENT';
+
+// Appointment Constant
+export const GET_APPOINTMENT = 'GET_APPOINTMENT';
+export const APPOINTMENT_ERROR = 'APPOINTMENT_ERROR';
+export const CREATE_APPOINTMENT = 'CREATE_APPOINTMENT';
