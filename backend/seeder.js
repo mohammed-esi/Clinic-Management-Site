5b3562e4d34effee1b3daac4b01b0f7a97edf710
Mohammed Elamine <51944814+mohammed-esi@users.noreply.github.com>
2021-06-05 22:45:30 +0200

    add more association between tables

--- backend/seeder.js
+++ backend/seeder.js
@@ -4,6 +4,10 @@ const db = require('./config/db');
 const User = require('./models/User');
 const Patient = require('./models/Patient');
 const Appointment = require('./models/Appointment');
+const Medicament = require('./models/Medicament');
+const PrescribedMedicament = require('./models/PrescribedMedicament');
+const Prescription = require('./models/Prescription');
+const Consultation = require('./models/Consultation');
 
 const { DOCTOR, SECRETARY } = require('./utils/roles');
 
@@ -46,9 +50,45 @@ db.sync().then(async () => {
   Appointment.belongsTo(User, { foreignKey: 'user_id' });
   User.hasOne(Appointment, { foreignKey: 'user_id' });
 
+  // Medicament - User
+  Medicament.belongsTo(User, { foreignKey: 'user_id' });
+  User.hasOne(Medicament, { foreignKey: 'user_id' });
+
+  // Prescribed Medicament - User
+  PrescribedMedicament.belongsTo(User, { foreignKey: 'user_id' });
+  User.hasOne(PrescribedMedicament, { foreignKey: 'user_id' });
+
+  // Prescription - User
+  Prescription.belongsTo(User, { foreignKey: 'user_id' });
+  User.hasOne(Prescription, { foreignKey: 'user_id' });
+
+  // Consultation - User
+  Consultation.belongsTo(User, { foreignKey: 'user_id' });
+  User.hasOne(Consultation, { foreignKey: 'user_id' });
+
   // Appointment - Patient
   Appointment.belongsTo(Patient, { foreignKey: 'patient_id' });
   Patient.hasOne(Appointment, { foreignKey: 'patient_id' });
+
+  // Consultation - Appointment
+  Consultation.belongsTo(Appointment, { foreignKey: 'app_id' });
+  Appointment.hasOne(Consultation, { foreignKey: 'app_id' });
+
+  // Prescribed Medicament - Medicament
+  PrescribedMedicament.belongsTo(Medicament, { foreignKey: 'medicament_id' });
+  Medicament.hasOne(PrescribedMedicament, { foreignKey: 'medicament_id' });
+
+  // Prescription - Prescribed Medicament
+  Prescription.belongsTo(PrescribedMedicament, {
+    foreignKey: 'presc_medicament_id',
+  });
+  PrescribedMedicament.hasOne(Prescription, {
+    foreignKey: 'presc_medicament_id',
+  });
+
+  // Consultation - Prescription
+  Consultation.belongsTo(Prescription, { foreignKey: 'presc_id' });
+  Prescription.hasOne(Consultation, { foreignKey: 'presc_id' });
 
   //#endregion Associations
 
