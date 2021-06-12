1bb6b0cf3f8e209e6cbda0efe3a801723493cab0
Mohammed Elamine <51944814+mohammed-esi@users.noreply.github.com>
2021-06-13 01:44:37 +0200

    map reducers

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -2,9 +2,17 @@ import { combineReducers } from 'redux';
 import patient from './patient';
 import appointment from './appointment';
 import auth from './auth';
+import medicament from './medicament';
+import consultation from './consultation';
+import prescription from './prescription';
+import prescribed_medicament from './prescribed_medicament';
 
 export default combineReducers({
   patient,
   appointment,
   auth,
+  medicament,
+  consultation,
+  prescription,
+  prescribed_medicament,
 });
