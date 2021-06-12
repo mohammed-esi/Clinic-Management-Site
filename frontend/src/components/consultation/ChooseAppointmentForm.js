4e40f126d4486ad4849a4eadd1a8d2c49c7cda5f
Mohammed Elamine <51944814+mohammed-esi@users.noreply.github.com>
2021-06-13 01:41:22 +0200

    form of choose appointment to get consultation

---
+++ frontend/src/components/consultation/ChooseAppointmentForm.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import SelectAppointmentInput from '../appointment/SelectAppointemntInput';
+
+function ChooseAppointmentForm(props) {
+  const [formData, setFormData] = useState({
+    app_id: '',
+  });
+
+  const { app_id } = formData;
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (app_id === '') {
+      return toast.error('Please choose one of appointments at selector!');
+    }
+    props.history.push(`/dashboard/create_consultation/${app_id}`);
+  };
+  return (
+    <div className='container'>
+      <div className='card'>
+        <div className='card-body'>
+          <form onSubmit={onSubmit}>
+            <div className='form-group'>
+              <h4>Choose Appointment</h4>
+              <select
+                name='app_id'
+                value={app_id}
+                className='form-control'
+                onChange={onChange}
+              >
+                <option value='' disabled>
+                  Choose...
+                </option>
+                <SelectAppointmentInput />
+              </select>
+            </div>
+            <div className='d-flex'>
+              <input
+                type='submit'
+                className='btn btn-primary'
+                value={'Create Consultation'}
+              />
+              <Link to='/dashboard' className='btn btn-info ml-2'>
+                Come Back
+              </Link>
+            </div>
+          </form>
+          <ToastContainer />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default withRouter(ChooseAppointmentForm);
