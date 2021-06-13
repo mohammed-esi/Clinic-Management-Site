9f01c3e5a24516c1e4b7275e84c28c2903a01a5d
Mohammed Elamine <51944814+mohammed-esi@users.noreply.github.com>
2021-06-13 23:17:27 +0200

    add delete button

--- frontend/src/components/prescribed_medicament/PrescribedMedicamentItem.js
+++ frontend/src/components/prescribed_medicament/PrescribedMedicamentItem.js
@@ -1,20 +1,45 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { deletePrescribedMedicament } from '../../actinos/prescribed_medicament';
 
-function PrescribedMedicamentItem({ prescribed_medicament }) {
+function PrescribedMedicamentItem({
+  prescribed_medicament,
+  deletePrescribedMedicament,
+}) {
   const {
+    id,
     dosage,
     medicament: { name },
   } = prescribed_medicament;
+
+  const onDelete = () => {
+    deletePrescribedMedicament(id);
+    toast.info('Prescribed medicament deleted !');
+  };
+
   return (
-    <li class='list-group-item'>
-      {name} / {dosage}
-    </li>
+    <>
+      <li class='list-group-item d-flex justify-content-between align-items-center'>
+        {name} / {dosage}
+        <button className='btn btn-danger' onClick={onDelete}>
+          <i class='fas fa-trash-alt'></i>
+        </button>
+      </li>
+      <ToastContainer />
+    </>
   );
 }
 
 PrescribedMedicamentItem.propTypes = {
   prescribed_medicament: PropTypes.object,
+  deletePrescribedMedicament: PropTypes.func.isRequired,
 };
 
-export default PrescribedMedicamentItem;
+const mapStateToProps = (state) => ({});
+
+export default connect(mapStateToProps, { deletePrescribedMedicament })(
+  PrescribedMedicamentItem
+);
