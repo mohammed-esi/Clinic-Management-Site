a82936f659fc1c0d258ae4d37b3bb67ef65de7b9
Mohammed Elamine <51944814+mohammed-esi@users.noreply.github.com>
2021-06-13 23:14:45 +0200

    add action delete, update, getById to consultation

--- frontend/src/actinos/consultation.js
+++ frontend/src/actinos/consultation.js
@@ -1,8 +1,12 @@
 import api from '../utils/api';
 import {
   GET_CONSULTATIONS,
+  GET_CONSULTATION_BY_ID,
   CONSULTATION_ERROR,
   CREATE_CONSULTATION,
+  DELETE_CONSULTATION,
+  UPDATE_CONSULTATION,
+  CLEAR_CONSULTATION,
 } from '../utils/constant';
 
 // Get Consultations
@@ -45,3 +49,60 @@ export const createConsultation = (formData) => async (dispatch) => {
     });
   }
 };
+
+// Get Consultation By Id
+export const getConsultationById = (id) => async (dispatch) => {
+  try {
+    const res = await api.get(`/api/consultations/${id}`);
+
+    dispatch({
+      type: GET_CONSULTATION_BY_ID,
+      payload: res.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: CONSULTATION_ERROR,
+      payload: err.message,
+    });
+  }
+};
+
+// Delete Consultation
+export const deleteConsultation = (id) => async (dispatch) => {
+  if (window.confirm('Are you sure? This can NOT be undone!')) {
+    try {
+      await api.delete(`/api/consultations/${id}`);
+
+      dispatch({
+        type: DELETE_CONSULTATION,
+        payload: id,
+      });
+    } catch (err) {
+      dispatch({
+        type: CONSULTATION_ERROR,
+        payload: err.message,
+      });
+    }
+  }
+};
+
+// Update Consultation
+export const updateConsultation = (id, formData) => async (dispatch) => {
+  try {
+    const res = await api.put(`/api/consultations/${id}`, formData);
+
+    dispatch({
+      type: UPDATE_CONSULTATION,
+      payload: res.data,
+    });
+  } catch (err) {
+    console.log(err);
+    dispatch({
+      type: CONSULTATION_ERROR,
+      payload: err.message,
+    });
+  }
+};
+
+// Clear Consultation
+export const clearConsultation = () => ({ type: CLEAR_CONSULTATION });
--- frontend/src/reducers/consultation.js
+++ frontend/src/reducers/consultation.js
@@ -1,7 +1,11 @@
 import {
   GET_CONSULTATIONS,
+  GET_CONSULTATION_BY_ID,
   CONSULTATION_ERROR,
   CREATE_CONSULTATION,
+  DELETE_CONSULTATION,
+  UPDATE_CONSULTATION,
+  CLEAR_CONSULTATION,
 } from '../utils/constant';
 
 const initialState = {
@@ -21,10 +25,32 @@ function consultationReducer(state = initialState, action) {
         consultations: payload,
         loading: false,
       };
+    case GET_CONSULTATION_BY_ID:
+      return {
+        ...state,
+        consultation: payload,
+      };
     case CONSULTATION_ERROR:
       return {
         ...state,
         error: payload,
+        loading: false,
+      };
+    case UPDATE_CONSULTATION:
+      return {
+        ...state,
+        consultations: state.consultations.filter((consultation) =>
+          consultation.id === payload.id
+            ? { motif: payload.motif, observation: payload.observation }
+            : null
+        ),
+      };
+    case DELETE_CONSULTATION:
+      return {
+        ...state,
+        consultations: state.consultations.filter(
+          (consultation) => consultation.id !== payload
+        ),
         loading: false,
       };
     case CREATE_CONSULTATION:
@@ -32,6 +58,11 @@ function consultationReducer(state = initialState, action) {
         ...state,
         consultations: [payload, ...state.consultations],
         loading: false,
+      };
+    case CLEAR_CONSULTATION:
+      return {
+        ...state,
+        consultation: {},
       };
     default:
       return state;
