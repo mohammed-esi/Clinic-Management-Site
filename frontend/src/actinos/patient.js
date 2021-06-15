8081f0c38af240fc502945dc3966503669105d28
Mohammed Elamine <51944814+mohammed-esi@users.noreply.github.com>
2021-06-16 00:38:54 +0200

    actions, reducers patient

--- frontend/src/actinos/patient.js
+++ frontend/src/actinos/patient.js
@@ -1,5 +1,13 @@
 import api from '../utils/api';
-import { GET_PATIENTS, PAITENT_ERROR, CREATE_PATIENT } from '../utils/constant';
+import {
+  GET_PATIENTS,
+  PAITENT_ERROR,
+  CREATE_PATIENT,
+  DELETE_PATIENT,
+  GET_PATIENT_BY_ID,
+  CLEAR_PATIENT,
+  UPDATE_PATIENT,
+} from '../utils/constant';
 
 // Get Patients
 export const getPatients = () => async (dispatch) => {
@@ -20,17 +28,76 @@ export const getPatients = () => async (dispatch) => {
 
 // Add Patient
 export const addPatient = (formData) => async (dispatch) => {
-  try {
-    const res = await api.post('/api/patients', formData);
+  const res = await api.post('/api/patients', formData);
+  console.log(res.data);
 
+  if (res.data.status_code === 200) {
     dispatch({
       type: CREATE_PATIENT,
       payload: res.data,
     });
+  } else {
+    console.log(res.data);
+    dispatch({
+      type: PAITENT_ERROR,
+      payload: res.data.msg,
+    });
+  }
+};
+
+// Delete patient
+export const deletePatient = (id) => async (dispatch) => {
+  try {
+    const res = await api.delete(`/api/patients/${id}`);
+
+    dispatch({
+      type: DELETE_PATIENT,
+      payload: id,
+    });
   } catch (err) {
+    console.log(err);
     dispatch({
       type: PAITENT_ERROR,
       payload: err.message,
     });
   }
 };
+
+// Get Patient By Id
+export const getPatientById = (id) => async (dispatch) => {
+  try {
+    const res = await api.get(`/api/patients/${id}`);
+
+    dispatch({
+      type: GET_PATIENT_BY_ID,
+      payload: res.data,
+    });
+  } catch (err) {
+    console.log(err);
+    dispatch({
+      type: PAITENT_ERROR,
+      payload: err.message,
+    });
+  }
+};
+
+// Update patient
+export const updatePatient = (id, formData) => async (dispatch) => {
+  try {
+    const res = await api.put(`/api/patients/${id}`, formData);
+
+    dispatch({
+      type: UPDATE_PATIENT,
+      payload: id,
+    });
+  } catch (err) {
+    console.log(err);
+    dispatch({
+      type: PAITENT_ERROR,
+      payload: err.message,
+    });
+  }
+};
+
+// Clear Consultation
+export const clearPatient = () => ({ type: CLEAR_PATIENT });
--- frontend/src/reducers/patient.js
+++ frontend/src/reducers/patient.js
@@ -1,10 +1,18 @@
-import { GET_PATIENTS, PAITENT_ERROR, CREATE_PATIENT } from '../utils/constant';
+import {
+  GET_PATIENTS,
+  PAITENT_ERROR,
+  CREATE_PATIENT,
+  DELETE_PATIENT,
+  GET_PATIENT_BY_ID,
+  CLEAR_PATIENT,
+  UPDATE_PATIENT,
+} from '../utils/constant';
 
 const initialState = {
   patients: [],
   patient: null,
   loading: true,
-  error: {},
+  error: null,
 };
 
 function patientReducer(state = initialState, action) {
@@ -17,17 +25,51 @@ function patientReducer(state = initialState, action) {
         patients: payload,
         loading: false,
       };
+    case GET_PATIENT_BY_ID:
+      return {
+        ...state,
+        patient: payload,
+      };
     case CREATE_PATIENT:
       return {
         ...state,
         patients: [payload, ...state.patients],
         loading: false,
       };
+    case DELETE_PATIENT:
+      return {
+        ...state,
+        patients: state.patients.filter((patient) => patient.id !== payload),
+        loading: false,
+      };
+    case UPDATE_PATIENT:
+      return {
+        ...state,
+        patients: state.patients.filter((patient) =>
+          patient.id === payload.id
+            ? {
+                first_name: payload.first_name,
+                last_name: payload.last_name,
+                age: payload.age,
+                sex: payload.sex,
+                city: payload.city,
+                email: payload.email,
+                phone_number: payload.phone_number,
+                blood_group: payload.blood_group,
+              }
+            : null
+        ),
+      };
     case PAITENT_ERROR:
       return {
         ...state,
         error: payload,
         loading: false,
+      };
+    case CLEAR_PATIENT:
+      return {
+        ...state,
+        patient: null,
       };
     default:
       return state;
